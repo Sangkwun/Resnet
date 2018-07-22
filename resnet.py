adbda033203714ea23aa586c9074edc7f048e3d4
Sangkwun <wer2774@gmail.com>
2018-07-22 17:59:34 +0900

    make resnet of paper

--- resnet.py
+++ resnet.py
@@ -5,6 +5,7 @@ def conv_block(input_layer,
                    kernel_size,
                    filters,
                    strides=(2,2),
+                   first_strides=None,
                    first_projection=True,
                    block_n=3,
                    name=None
@@ -12,32 +13,39 @@ def conv_block(input_layer,
 
         filter_1, filter_2, filter_3 = filters
         bn_axis = 3
-        
-        for i in range(block_n):
+
+        if first_strides is None:
+            first_strides = strides
+
+        for i in range(1, block_n + 1):
+            if i > 1:
+                first_strides = (1, 1)
+
             # First conv layer of Block
-            x = Conv2D(filter_1, kernel_size=(1,1), strides=strides, name="{}_{}_conv1".format(name, i))(input_layer)
-            x = BatchNormalization(axis=bn_axis, name="{}_1_bn".format(name))(x)
-            x = layers.Activation('relu')(x)
+            x = Conv2D(filter_1, kernel_size=(1,1), strides=first_strides, name="{}_{}_conv1".format(name, i))(input_layer)
+            x = BatchNormalization(axis=bn_axis, name="{}_{}_bn1".format(name, i))(x)
+            x = Activation('relu')(x)
 
             # Second conv layer of Block
-            x = Conv2D(filter_2, kernel_size, strides=(1,1), padding='SAME', name="{}_{}_conv2".format(name, i))(x)
-            x = BatchNormalization(axis=bn_axis, name="{}_2_bn".format(name))(x)
-            x = layers.Activation('relu')(x)
+            x = Conv2D(filter_2, kernel_size, strides=(1,1), padding='same', name="{}_{}_conv2".format(name, i))(x)
+            x = BatchNormalization(axis=bn_axis, name="{}_{}_bn2".format(name, i))(x)
+            x = Activation('relu')(x)
 
             # Third conv layer of Block
-            x = Conv2D(filter_3, kernel_size=(1,1), strides=(1,1), name="{}_{}_conv3".format(name, i))(x)
-            x = BatchNormalization(axis=bn_axis, name="{}_3_bn".format(name))(x)
-            x = layers.Activation('relu')(x)
+            x = Conv2D(filter_3, kernel_size=(1,1), name="{}_{}_conv3".format(name, i))(x)
+            x = BatchNormalization(axis=bn_axis, name="{}_{}_bn3".format(name, i))(x)
+            x = Activation('relu')(x)
             
-            if first_projection and i = 0:
+            # shortcut part
+            if first_projection and i == 1:
                 # projection shortcut for extension
-                short_con = Conv2D(filter_3, strides=strides, name="{}_{}_short".format(name, i))(input_layer)
-                short_con = BatchNormalization(axis=bn_axis, name="{}_{}_bn".format(name, i))(short_con)
+                shortcut = Conv2D(filter_3, kernel_size=(1,1), strides=first_strides, name="{}_{}_short".format(name, i))(input_layer)
+                shortcut = BatchNormalization(axis=bn_axis, name="{}_{}_bn4".format(name, i))(shortcut)
             else:
                 # identity short cut
-                short_con = input_layer
+                shortcut = input_layer
 
-            x = add([x, shortcut])
+            x = add([x, shortcut], name="{}_{}_add".format(name, i))
             x = Activation('relu')(x)
 
             input_layer = x
@@ -55,14 +63,17 @@ def resnet(input_shape=None):
     inputs = Input(shape=_shape, name="input_layer")
 
     # 112 x 112 x 64
-    x = Conv2D(filters=64, kernel_size=(7, 7), strides=(2, 2), padding='valid')(inputs)
-    x = BatchNormalization(axis=bn_axis, name="{}_1_bn".format(name))(x)
-    x = layers.Activation('relu')(x)
-    x = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), padding='valid')(conv1)
+    x = Conv2D(filters=64, kernel_size=(7, 7), strides=(2, 2), padding='same')(inputs)
+    x = BatchNormalization(axis=3)(x)
+    x = Activation('relu')(x)
+    x = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), padding='same', name="stage2_0_maxpooling")(x)
 
+    x = conv_block(input_layer=x, kernel_size=(3, 3), strides=(2, 2), first_strides=(1, 1), filters=[64, 64, 256], name="stage2")
+    x = conv_block(input_layer=x, kernel_size=(3, 3), strides=(2, 2), filters=[128, 128, 512], block_n=4, name="stage3")
+    x = conv_block(input_layer=x, kernel_size=(3, 3), strides=(2, 2), filters=[256, 256, 1024], block_n=6, name="stage4")
+    x = conv_block(input_layer=x, kernel_size=(3, 3), strides=(2, 2), filters=[512, 512, 2048], block_n=3, name="stage5")
 
-
-    model = Model(inputs=inputs, outputs=conv1)
+    model = Model(inputs=inputs, outputs=x)
     model.summary()
 
 
