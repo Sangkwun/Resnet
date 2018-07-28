cf5719a8d6d1fc29e6716d353023afd640d04abd
Sangkwun <wer2774@gmail.com>
2018-07-28 13:25:36 +0900

    add tensorboard to record log

--- train.py
+++ train.py
@@ -3,11 +3,12 @@ import resnet
 from keras.datasets import cifar10
 from keras.optimizers import Adam
 from keras.utils import to_categorical
-from keras.callbacks import EarlyStopping
+from keras.callbacks import EarlyStopping, TensorBoard
 
 model = resnet.resnet(input_shape=(32, 32, 3), classes=10)
 model.summary()
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+tensorboard = TensorBoard(log_dir='./graph', histogram_freq=0, write_graph=True, write_images=True)
 
 # Compile the model
 model.compile(loss='categorical_crossentropy',
@@ -20,7 +21,7 @@ model.fit(x_train / 255.0, to_categorical(y_train),
             shuffle=True,
             epochs=250,
             validation_data=(x_test / 255.0, to_categorical(y_test)),
-            callbacks=[EarlyStopping(min_delta=0.001, patience=3)])
+            callbacks=[EarlyStopping(min_delta=0.001, patience=3), tensorboard])
 
 # Evaluate the model
 scores = model.evaluate(x_test / 255.0, to_categorical(y_test))
